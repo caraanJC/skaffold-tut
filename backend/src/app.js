7396c6ceb3bb4c8c5d785a2ea30f8e1a786d8202
John Carlo Caraan <caraanjohncarlo@gmail.com>
2024-06-26 20:50:03 +0800

    feat: add questions

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,7 +3,30 @@
 const express = require('express')
 const app = express()
 
+const nunjucks = require('nunjucks');
+const questions = require('./config/questions');
+nunjucks.configure("views", {
+    autoescape: true,
+    express: app
+});
+
+app.use(express.json())
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.static('public'));
-app.get('/hello', (req, res) => res.send('Hello World v2'))
+app.get('/', (req, res) => {
+    res.render("pages/home.html", {
+        questions
+    })
+})
+
+app.post('/answer', (req, res) => {
+    let response = `<button type="submit" id="submit-{{ loop.index }}">Submit</button>`
+    if (req.body.answer.includes('-ans')) {
+        response = `Correct`
+    }
+    res.status(200).send(response)
+})
 
 module.exports = app
